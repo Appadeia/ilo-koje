6b788b441862cea29ae272b85eef99272380663f
PieZu <39189047+PieZu@users.noreply.github.com>
2020-01-24 21:39:27 +1300

    allow user to choose font
    usage: k!s <words>, {kepeken|font} <fontname>
    to add fonts to the bot, install them on the host system then add a case in the validFont func.

--- koje/sitelenpona.go
+++ koje/sitelenpona.go
@@ -19,6 +19,16 @@ func randSeq(n int) string {
 	return string(b)
 }
 
+func validFont(name string) bool {
+	switch name{
+	case
+		"linja pona",
+		"linja pimeja":
+		return true
+    	}
+    return false
+}
+
 func sitelenPona(s *discordgo.Session, m *discordgo.MessageCreate) {
 	lex := strings.Split(strings.Split(m.Content, "!")[1], " ")
 
@@ -31,13 +41,32 @@ func sitelenPona(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	text := ""
-	for _, word := range lex[1:] {
-		text = text + " " + word
-		text = strings.TrimSpace(text)
+	text := strings.Join(lex[1:], " ")
+	fontname := "linja pimeja" // default font
+	fontsize := "50"
+	
+	extra := strings.Split(text, ", ")"
+	if len(extra) > 1 {
+		lastextra := extra[len(extra)-1]
+		extra = strings.SplitN(lastextra, " ", 2) // = ["first word after last comma", "rest of text after last comma"]
+		if len(extra)==2 && (extra[0] == "kepeken" || extra[0] == "font") {
+			if validFont(extra[1]) {
+				fontname = extra[1]
+				text = text[:len(text)-len(lastextra)-2] // dont render the options
+			} else {
+				embed := NewEmbed().
+					SetTitle(_t("Error!", "pakala!", m)).
+					SetDescription(_t("Unrecognised font: "+extra[1], "mi jo ala e sitelen pi "+extra[1], m)).
+					SetColor(0xff0000)
+				s.ChannelMessageSendEmbed(m.ChannelID, embed.MessageEmbed)
+				return
+			}
+		}
 	}
+	
+	
 	filename := "/tmp/" + randSeq(10) + ".png"
-	cmd := exec.Command("pango-view", "-t", text, "--font", "linja pimeja 50", "-o", filename, "-q", "--align=center", "--hinting=slight", "--antialias=gray", "--margin=10px")
+	cmd := exec.Command("pango-view", "-t", text, "--font", fontname+" "+fontsize, "-o", filename, "-q", "--align=center", "--hinting=slight", "--antialias=gray", "--margin=10px")
 	if err := cmd.Run(); err != nil {
 		embed := NewEmbed().
 			SetTitle("Internal Error").
